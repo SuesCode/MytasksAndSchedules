392f5ad22da1dfee206cef4dcf18c321c42514e7
Sue <susmitha.murali@gmail.com>
2025-02-01 20:33:13 -0500

    Some unicode values
    Some unicode values

--- MytasksAndSchedules/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MytasksAndSchedules.AssemblyInfo.cs
+++ MytasksAndSchedules/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/MytasksAndSchedules.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MytasksAndSchedules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+ef8d9b68260601f155e8cc3ceef815ca42b7cec1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+3736976d87b051b36ffd8af442ee5f36067ec5e5")]
 [assembly: System.Reflection.AssemblyProductAttribute("MytasksAndSchedules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MytasksAndSchedules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
